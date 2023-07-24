874f57c8cd134d97785e85352a774bf2d87a2020
kmor76 <kmor76@yandex.ru>
2023-07-24 19:01:33 +0300

    Initial commit

---
+++ spark-thrift.sh
@@ -0,0 +1,4 @@
+../bin/spark-shell \
+--conf spark.hadoop.fs.s3a.access.key=CR5JHr9b5MF7sE8Ga06K \
+--conf spark.hadoop.fs.s3a.secret.key=jhP7SlTMigczfXQDtvqnAwOnTce7MpyXOKpTjeYi \
+--conf spark.hadoop.fs.s3a.endpoint=minio:9000
